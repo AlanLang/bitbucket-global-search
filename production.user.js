26beeb33cf015516d3cbe1dac3adfeaf58a78a9c
AlanLang <langwdalan@gmail.com>
2022-11-15 03:50:45 +0000

    Update tampermonkey to output generated at f7db2c7

--- production.user.js
+++ production.user.js
@@ -9,7 +9,129 @@
 // @license MIT
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
-(()=>{"use strict";var b={};function h(){return new Promise((t,o)=>{GM_xmlhttpRequest({method:"GET",url:"https://code.fineres.com/rest/api/latest/profile/recent/repos?avatarSize=64&limit=10",headers:{Accept:"application/json, text/javascript, */*; q=0.01","Accept-Language":"zh,zh-CN;q=0.9,en-US;q=0.8,en;q=0.7","Cache-Control":"no-cache",Connection:"keep-alive","Content-Type":"application/json","X-Requested-With":"XMLHttpRequest",cookie:document.cookie},data:"",onload(e){const i=JSON.parse(e.responseText);t(i)},onerror(e){console.error("\u8BF7\u6C42\u5931\u8D25:"),console.error(e),o(e)}})})}function v(){return new Promise((t,o)=>{GM_xmlhttpRequest({method:"GET",url:"https://code.fineres.com/rest/categories/latest/projects?start=0&limit=5000&project=",headers:{Accept:"application/json, text/javascript, */*; q=0.01","Accept-Language":"zh,zh-CN;q=0.9,en-US;q=0.8,en;q=0.7","Cache-Control":"no-cache",Connection:"keep-alive","Content-Type":"application/json","X-Requested-With":"XMLHttpRequest",cookie:document.cookie},data:"",onload(e){const i=JSON.parse(e.responseText);t(i)},onerror(e){console.error("\u8BF7\u6C42\u5931\u8D25:"),console.error(e),o(e)}})})}function g(t){return new Promise((o,e)=>{GM_xmlhttpRequest({method:"GET",url:`https://code.fineres.com/rest/api/latest/projects/${t}/repos?avatarSize=64&limit=5000`,headers:{Accept:"application/json, text/javascript, */*; q=0.01","Accept-Language":"zh,zh-CN;q=0.9,en-US;q=0.8,en;q=0.7","Cache-Control":"no-cache",Connection:"keep-alive","Content-Type":"application/json","X-Requested-With":"XMLHttpRequest",cookie:document.cookie},data:"",onload(i){const n=JSON.parse(i.responseText);o(n)},onerror(i){console.error("\u8BF7\u6C42\u5931\u8D25:"),console.error(i),e(i)}})})}var c=(t,o,e)=>new Promise((i,n)=>{var l=a=>{try{s(e.next(a))}catch(p){n(p)}},r=a=>{try{s(e.throw(a))}catch(p){n(p)}},s=a=>a.done?i(a.value):Promise.resolve(a.value).then(l,r);s((e=e.apply(t,o)).next())});let u=[];function m(){const t=document.querySelector(".aui-nav"),o=document.createElement("a");o.innerText="\u5168\u5C40\u641C\u7D22",o.style.cursor="pointer";const e=document.createElement("li");return e.appendChild(o),t==null||t.appendChild(e),o}function f(){y()}function y(){const t=document.createElement("div");return t.innerHTML=`
+(() => {
+  "use strict";
+  var __webpack_exports__ = {};
+  ;
+  function getLastReps() {
+    return new Promise((resolve, reject) => {
+      GM_xmlhttpRequest({
+        method: "GET",
+        url: "https://code.fineres.com/rest/api/latest/profile/recent/repos?avatarSize=64&limit=10",
+        headers: {
+          Accept: "application/json, text/javascript, */*; q=0.01",
+          "Accept-Language": "zh,zh-CN;q=0.9,en-US;q=0.8,en;q=0.7",
+          "Cache-Control": "no-cache",
+          Connection: "keep-alive",
+          "Content-Type": "application/json",
+          "X-Requested-With": "XMLHttpRequest",
+          cookie: document.cookie
+        },
+        data: ``,
+        onload(response) {
+          const result = JSON.parse(response.responseText);
+          resolve(result);
+        },
+        onerror(response) {
+          console.error("\u8BF7\u6C42\u5931\u8D25:");
+          console.error(response);
+          reject(response);
+        }
+      });
+    });
+  }
+  function getAllPorjects() {
+    return new Promise((resolve, reject) => {
+      GM_xmlhttpRequest({
+        method: "GET",
+        url: "https://code.fineres.com/rest/categories/latest/projects?start=0&limit=5000&project=",
+        headers: {
+          Accept: "application/json, text/javascript, */*; q=0.01",
+          "Accept-Language": "zh,zh-CN;q=0.9,en-US;q=0.8,en;q=0.7",
+          "Cache-Control": "no-cache",
+          Connection: "keep-alive",
+          "Content-Type": "application/json",
+          "X-Requested-With": "XMLHttpRequest",
+          cookie: document.cookie
+        },
+        data: ``,
+        onload(response) {
+          const result = JSON.parse(response.responseText);
+          resolve(result);
+        },
+        onerror(response) {
+          console.error("\u8BF7\u6C42\u5931\u8D25:");
+          console.error(response);
+          reject(response);
+        }
+      });
+    });
+  }
+  function getReposByProject(project) {
+    return new Promise((resolve, reject) => {
+      GM_xmlhttpRequest({
+        method: "GET",
+        url: `https://code.fineres.com/rest/api/latest/projects/${project}/repos?avatarSize=64&limit=5000`,
+        headers: {
+          Accept: "application/json, text/javascript, */*; q=0.01",
+          "Accept-Language": "zh,zh-CN;q=0.9,en-US;q=0.8,en;q=0.7",
+          "Cache-Control": "no-cache",
+          Connection: "keep-alive",
+          "Content-Type": "application/json",
+          "X-Requested-With": "XMLHttpRequest",
+          cookie: document.cookie
+        },
+        data: ``,
+        onload(response) {
+          const result = JSON.parse(response.responseText);
+          resolve(result);
+        },
+        onerror(response) {
+          console.error("\u8BF7\u6C42\u5931\u8D25:");
+          console.error(response);
+          reject(response);
+        }
+      });
+    });
+  }
+  ;
+  var __async = (__this, __arguments, generator) => {
+    return new Promise((resolve, reject) => {
+      var fulfilled = (value) => {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      };
+      var rejected = (value) => {
+        try {
+          step(generator.throw(value));
+        } catch (e) {
+          reject(e);
+        }
+      };
+      var step = (x) => x.done ? resolve(x.value) : Promise.resolve(x.value).then(fulfilled, rejected);
+      step((generator = generator.apply(__this, __arguments)).next());
+    });
+  };
+  let cacheResult = [];
+  function appendEntryButton() {
+    const nav = document.querySelector(".aui-nav");
+    const a = document.createElement("a");
+    a.innerText = "\u5168\u5C40\u641C\u7D22";
+    a.style.cursor = "pointer";
+    const li = document.createElement("li");
+    li.appendChild(a);
+    nav == null ? void 0 : nav.appendChild(li);
+    return a;
+  }
+  function init() {
+    appendDialog();
+  }
+  function appendDialog() {
+    const dialog = document.createElement("div");
+    dialog.innerHTML = `
     <div class="aui-dialog2 aui-dialog2-large aui-dialog2-current aui-layer" id="aui-dialog2-1" role="dialog" aria-labelledby="aui-dialog2-1-heading" aria-hidden="true">
       <div class="aui-dialog2-header">
         <h2 class="aui-dialog2-header-main" id="aui-dialog2-1-heading">\u5168\u5C40\u641C\u7D22</h2>
@@ -36,4 +158,82 @@
         </div>
       </div>
     </div>
-  `,document.body.appendChild(t),AJS.$("#close-btn").click(()=>{AJS.dialog2("#aui-dialog2-1").hide()}),AJS.$(".aui-iconfont-close-dialog").click(()=>{AJS.dialog2("#aui-dialog2-1").hide()}),t}function d(t){const o=document.querySelector("#search-result-repositories");o&&(o.innerHTML=`${t.map(e=>`<li><a href="/projects/${e.project.key}/repos/${e.name}/browse" aria-label="${e.project.name} / ${e.name}"><div class="project-avatar"><div style="display: inline-block; position: relative; outline: 0px;"><span class="css-50fm9s"><span class="css-1gv8fjs" role="img" aria-label="" style="background-image: url('${e.project.avatarUrl}');height: 40px;background-size: contain;"></span></span></div></div><div class="repository-details"><div class="repository-name">${e.name}</div><div class="project-name">${e.project.name}</div></div></a></li>`).join("")}`)}function C(){const t=document.querySelector("#search-result-repositories");t&&(t.innerHTML='<div class="loading"><div class="loading-spinner" style="height: 100px;width: 100%;display: flex;justify-content: center;align-items: center;"><aui-spinner></aui-spinner></div></div>')}function j(){const t=document.querySelector("#search-result-repositories");t&&(t.innerHTML='<div style="height: 100px;width: 100%;display: flex;justify-content: center;align-items: center;" class="empty">\u6682\u65E0\u6570\u636E</div>')}function q(t,o){return c(this,null,function*(){let e=u;if(e.length===0){const l=(yield v()).result;for(let r=0;r<l.length;r++){const s=l[r];console.log(`\u6B63\u5728\u641C\u7D22${s.projectName}\u7684\u4ED3\u5E93`);const a=yield g(s.projectKey);e=[...e,...a.values]}u=e}const i=e.filter(n=>n.name.toLowerCase().includes(t.toLowerCase()));i.length>0?o(i):j()})}function x(){return c(this,null,function*(){u=[],f(),m().addEventListener("click",()=>c(this,null,function*(){const t=document.getElementById("search");t.value="",t.focus();const{values:o}=yield h();AJS.dialog2("#aui-dialog2-1").show(),d(o),AJS.$("#search-button").click(()=>c(this,null,function*(){const e=t.value;C(),e||d(o),yield q(e,d)}))}))})}setTimeout(()=>{x()},500)})();
+  `;
+    document.body.appendChild(dialog);
+    AJS.$("#close-btn").click(() => {
+      AJS.dialog2("#aui-dialog2-1").hide();
+    });
+    AJS.$(".aui-iconfont-close-dialog").click(() => {
+      AJS.dialog2("#aui-dialog2-1").hide();
+    });
+    return dialog;
+  }
+  function renderSearchResult(items) {
+    const repositories = document.querySelector("#search-result-repositories");
+    if (repositories) {
+      repositories;
+      repositories.innerHTML = `${items.map((item) => `<li><a href="/projects/${item.project.key}/repos/${item.name}/browse" aria-label="${item.project.name} / ${item.name}"><div class="project-avatar"><div style="display: inline-block; position: relative; outline: 0px;"><span class="css-50fm9s"><span class="css-1gv8fjs" role="img" aria-label="" style="background-image: url('${item.project.avatarUrl}');height: 40px;background-size: contain;"></span></span></div></div><div class="repository-details"><div class="repository-name">${item.name}</div><div class="project-name">${item.project.name}</div></div></a></li>`).join("")}`;
+    }
+  }
+  function renderLoading() {
+    const repositories = document.querySelector("#search-result-repositories");
+    if (repositories) {
+      repositories.innerHTML = `<div class="loading"><div class="loading-spinner" style="height: 100px;width: 100%;display: flex;justify-content: center;align-items: center;"><aui-spinner></aui-spinner></div></div>`;
+    }
+  }
+  function renderEmpty() {
+    const repositories = document.querySelector("#search-result-repositories");
+    if (repositories) {
+      repositories.innerHTML = `<div style="height: 100px;width: 100%;display: flex;justify-content: center;align-items: center;" class="empty">\u6682\u65E0\u6570\u636E</div>`;
+    }
+  }
+  function filterReps(keyWord, render) {
+    return __async(this, null, function* () {
+      let searchResult = cacheResult;
+      if (searchResult.length === 0) {
+        const result2 = yield getAllPorjects();
+        const projects = result2.result;
+        for (let index = 0; index < projects.length; index++) {
+          const project = projects[index];
+          console.log(`\u6B63\u5728\u641C\u7D22${project.projectName}\u7684\u4ED3\u5E93`);
+          const repo = yield getReposByProject(project.projectKey);
+          searchResult = [...searchResult, ...repo.values];
+        }
+        cacheResult = searchResult;
+      }
+      const result = searchResult.filter((v) => {
+        return v.name.toLowerCase().includes(keyWord.toLowerCase());
+      });
+      if (result.length > 0) {
+        render(result);
+      } else {
+        renderEmpty();
+      }
+    });
+  }
+  function main() {
+    return __async(this, null, function* () {
+      cacheResult = [];
+      init();
+      appendEntryButton().addEventListener("click", () => __async(this, null, function* () {
+        const search = document.getElementById("search");
+        search.value = "";
+        search.focus();
+        const { values } = yield getLastReps();
+        AJS.dialog2("#aui-dialog2-1").show();
+        renderSearchResult(values);
+        AJS.$("#search-button").click(() => __async(this, null, function* () {
+          const searchText = search.value;
+          renderLoading();
+          if (!searchText) {
+            renderSearchResult(values);
+          }
+          yield filterReps(searchText, renderSearchResult);
+        }));
+      }));
+    });
+  }
+  setTimeout(() => {
+    main();
+  }, 500);
+})();
